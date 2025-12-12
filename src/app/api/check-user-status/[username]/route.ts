7614a2616fde90aa8cfe9b6a4da6e2db91e1e03c
Ronak <ronakrathod0022@gmail.com>
2025-12-12 20:06:03 +0530

    add GET endpoint to check user status and return acceptance of messages

---
+++ src/app/api/check-user-status/[username]/route.ts
@@ -0,0 +1,32 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/Model/User.model";
+
+export async function GET(request: Request,{params}: {params: {username: string}}) {
+
+    const username = params.username
+    await dbConnect()
+
+    try {
+        const user = await UserModel.findOne({ username })
+
+        if (!user) {
+            return Response.json({
+                success: false,
+                message: "User not found"
+            }, { status: 404 })
+        }
+
+        return Response.json({
+            success: true,
+            isAcceptingMessages: user.isAcceptingMessages,
+            username: user.username
+        }, { status: 200 })
+
+    } catch (error) {
+        console.log("Error checking user status", error)
+        return Response.json({
+            success: false,
+            message: "Error checking user status"
+        }, { status: 500 })
+    }
+}
