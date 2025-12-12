82a267e3128111dc147766986c5f831fc3a2dffc
Ronak <ronakrathod0022@gmail.com>
2025-12-12 20:07:14 +0530

    fix: improve Navbar styling and correct welcome message capitalization

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,13 +14,13 @@ const Navbar = () => {
   const user: User = session?.user as User // this user is from session callback
 
   return (
-    <nav className='p-4 md:p-6 shadow-md'>
+    <nav className='p-4 md:p-6 shadow-md  bg-gray-300'>
       <div className='conatainer mx-auto flex flex-col md:flex-row justify-between items-center'>
         <a className="text-xl font-bold mb-4 md:mb-0" href="#">FeedBackify</a>
         {
           session ? (
             <>
-            <span className='mr-4'>WelCome, {user?.username || user?.email}</span>
+            <span className='mr-4 text-lg font-semibold'>Welcome, {user?.username || user?.email}</span>
             <Button className="w-full md:w-auto" onClick={() => signOut()}>Logout</Button>
             </>
           ) : (
